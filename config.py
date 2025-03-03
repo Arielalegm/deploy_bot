78200afc8cfabf7d2059ca01c54f1f18fc73bed1
Ariel Alejandro Gálvez Mirabal <144627936+Arielalegm@users.noreply.github.com>
2025-03-03 00:06:35 -0500

    1

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv()
 
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY')
+TELEGRAM_TOKEN = os.getenv('7522148561:AAFNUN_qD-oCjRs3BGM084AJPBwyN1T2HCQ')
+OPENROUTER_API_KEY = os.getenv('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')
 SITE_URL = "https://yoursiteurl.com"
 SITE_NAME = "DeepSeekBot"
 MAX_MEMORY_MESSAGES = 5
